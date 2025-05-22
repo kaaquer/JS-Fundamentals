9fe11b29ab3c11ccce6b6d558c6ba556065b1941
kaaquer <kaaquer2005@gmail.com>
2025-05-22 15:20:03 +0000

    8-square.js

--- 8-square.js
+++ 8-square.js
@@ -1,6 +1,8 @@
 const {argv} = require('process');
 input = parseInt(argv[2]);
 
-
-for (let i = 1; i <= input; i++) {
-        console.log('X'.repeat(input));}
+if (typeof input === 'number' && !isNaN(input) && input > 0) {
+    for (let i = 1; i <= input; i++) {
+        console.log('X'.repeat(input));}}
+else {
+    console.log('Missing size')}
